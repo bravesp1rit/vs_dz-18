c4a0e2e6a3a7f2721583941ed07dd8f696a022c6
brave <progpavlovmark@gmail.com>
2023-02-20 21:16:56 +0300

    dz-18 1.02 optimization

--- js/script.js
+++ js/script.js
@@ -1,48 +1,45 @@
 'use strict';
 
-const padString = function (string, num, symbol, boolean = true){
+const padString = function (string, strLength, symbol, right = true) {
     let result = string;
     let resultIfFalse = '';
-    if (string === undefined || typeof string != 'string' || string.trim() === ''){
+    if (typeof string != 'string' || string.trim() === '') {
         return 'error, missing string';
     }
-    if (num === undefined || typeof num  != 'number'){
+    if (typeof strLength != 'number') {
         return 'error, missing number';
     }
-    if (typeof boolean != 'boolean'){
-        return 'error, is not boolean';
+    if (typeof right != 'boolean') {
+        return 'error, is not right';
     }
-    if (num < string.length && boolean){
-        return string.substring(0, num);
-    } else if (num < string.length && !boolean){
-        return string.substring(string.length - num);
+    if (strLength < string.length && right) {
+        return string.substring(0, strLength);
+    } else if (strLength < string.length && !right) {
+        return string.substring(string.length - strLength);
     }
-    if (typeof symbol != 'string' || symbol.trim().length > 1){
-        return 'error, symbol is not string or greater then 1';
-    }
-    if (string.length == num && boolean || string.length == num && !boolean){
+    if (string.length == strLength && right || string.length == strLength && !right) {
         return result;
     }
-    for (let i = string.length; i < num; ++i){
-        if (boolean){
+    if (typeof symbol != 'string') return 'error, symbol is not string';
+    if (symbol.trim().length > 1) return 'error, symbol greater then 1';
+    for (let i = string.length; i < strLength; ++i) {
+        if (right) {
             result += symbol;
         } else {
             resultIfFalse += symbol;
-            if(resultIfFalse.length + string.length == num){
+            if (resultIfFalse.length + string.length == strLength) {
                 resultIfFalse += string;
                 break;
             }
         }
     }
-    if (boolean){
-        return result;
-    } else {
-        return resultIfFalse;
-    }
+    if (right) return result;
+    return resultIfFalse;
     
+
 };
 console.log(padString('hello', 3, '*', false));
-console.log(padString('hello', 8, '`'));
+console.log(padString('hello', 10, '`'));
 console.log(padString('string', 3));
 console.log(padString('qwerty', 3, '=', false));
 console.log(padString('qwerty', 3, '='));
